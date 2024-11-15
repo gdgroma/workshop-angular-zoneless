6657b7fe8c113fcfb5cbc4667ed717b4af1bac63
giorgiogalassi <ged.galassi@gmail.com>
2024-11-15 22:47:39 +0100

    fix: remove items dir

--- src/app/pages/items/items.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'app-shop',
-  template: `Items works!`,
-  styleUrls: ['./items.component.scss'],
-})
-export default class ItemsComponent {}
