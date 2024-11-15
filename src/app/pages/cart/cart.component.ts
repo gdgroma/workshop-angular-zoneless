9106cdd0b4acfa23dcba51932b9a69abd44d40df
Davide Passafaro <passafaro@hotmail.it>
2024-11-15 22:26:44 +0100

    refactor: removed async from cart component

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,21 +1,29 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { AsyncPipe, JsonPipe } from "@angular/common";
 
-import { TicketService } from '../../shared/providers/ticket.service';
-import { TicketComponent } from '../../shared/components/ticket/ticket.component';
-import { Ticket } from '../../shared/models/ticket';
+import { TicketService } from "../../shared/providers/ticket.service";
+import { TicketComponent } from "../../shared/components/ticket/ticket.component";
+import { Ticket } from "../../shared/models/ticket";
+import { Cart } from "../../shared/models/cart";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   standalone: true,
   imports: [TicketComponent, AsyncPipe, JsonPipe],
-  selector: 'app-shop',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss'],
+  selector: "app-shop",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.scss"],
 })
 export default class CartComponent {
   #ticketService = inject(TicketService);
 
-  cart$ = this.#ticketService.cart;
+  cart: Cart[] = [];
+
+  constructor() {
+    this.#ticketService.cart
+      .pipe(takeUntilDestroyed())
+      .subscribe((cart) => (this.cart = cart));
+  }
 
   removeTicket({ id }: Ticket): void {
     this.#ticketService.removeTicket(id);
