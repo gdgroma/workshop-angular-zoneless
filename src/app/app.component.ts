94cd1f268293aef0bcc0879d2ff2c401b3f9200d
Davide Passafaro <passafaro@hotmail.it>
2024-11-15 22:24:36 +0100

    refactor: removed async from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,39 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet} from '@angular/router';
-import { TicketService } from './shared/providers/ticket.service';
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import { Router, RouterOutlet } from "@angular/router";
+import { TicketService } from "./shared/providers/ticket.service";
+import { AsyncPipe, CurrencyPipe } from "@angular/common";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet, AsyncPipe, CurrencyPipe],
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
   router = inject(Router);
 
   #ticketService = inject(TicketService);
-  quantity$ = this.#ticketService.quantity;
-  price$ = this.#ticketService.price;
+
+  quantity = 0;
+  price = 0;
+
+  constructor() {
+    this.#ticketService.quantity
+      .pipe(takeUntilDestroyed())
+      .subscribe((quantity) => (this.quantity = quantity));
+
+    this.#ticketService.price
+      .pipe(takeUntilDestroyed())
+      .subscribe((price) => (this.price = price));
+  }
 
   goToCart(): void {
-    this.router.navigate(['/cart']);
+    this.router.navigate(["/cart"]);
   }
 
   goToShop(): void {
-    this.router.navigate(['/shop']);
+    this.router.navigate(["/shop"]);
   }
 }
